ab4404ee2119dddaa4caee2535ec1475029138e2
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-07-17 16:08:05 -0600

    Remove unnecessary spaces & comments

--- src/main.js
+++ src/main.js
@@ -1,49 +1,37 @@
-// Create variables targetting the relevant DOM elements here 👇
 var coverImage = document.querySelector('img');
 var coverTitle = document.querySelector('h2');
 var descriptor1 = document.querySelector('.tagline-1');
 var descriptor2 = document.querySelector('.tagline-2');
-
 var homeBtn = document.querySelector('.home-button');
 var rdmCoverBtn = document.querySelector('.random-cover-button');
 var saveCoverBtn = document.querySelector('.save-cover-button');
 var viewSavedBtn = document.querySelector('.view-saved-button');
 var makeNewBtn = document.querySelector('.make-new-button');
 var userBookBtn = document.querySelector('.create-new-book-button');
-
 var homeView = document.querySelector('.home-view');
 var makeMyBookView = document.querySelector('.form-view');
 var savedCoversView = document.querySelector('.saved-view');
-
 var userCover = document.getElementById('cover');
 var userTitle = document.getElementById('title');
 var userDescriptor1 = document.getElementById('descriptor1');
 var userDescriptor2 = document.getElementById('descriptor2');
-
 var viewSavedCovers = document.querySelector('.saved-covers-section')
-
-
-// We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-
 var currentCover;
 
-// Add your event listeners here 👇
 rdmCoverBtn.addEventListener('click', showRdmCover);
 makeNewBtn.addEventListener('click', switchViewsToForm);
 viewSavedBtn.addEventListener('click', switchViewsToSavedCovers);
 homeBtn.addEventListener('click', switchViewsToHome);
 window.addEventListener('load', showRdmCover);
 userBookBtn.addEventListener('click', saveUserInput);
-saveCoverBtn.addEventListener('click', saveUserCover);
+saveCoverBtn.addEventListener('click', saveCover);
+// viewSavedCovers.addEventListener('ondblclick', function() {
+//   deleteCover(this)
+// })
 
-
-// Create your event handlers and other functions here 👇
-
-
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
@@ -67,7 +55,6 @@ function switchViewsToForm() {
   homeView.classList.add('hidden');
   savedCoversView.classList.add('hidden');
   makeMyBookView.classList.remove('hidden');
-
   rdmCoverBtn.classList.add('hidden');
   saveCoverBtn.classList.add('hidden');
   homeBtn.classList.remove('hidden');
@@ -77,11 +64,9 @@ function switchViewsToSavedCovers() {
   homeView.classList.add('hidden');
   savedCoversView.classList.remove('hidden');
   makeMyBookView.classList.add('hidden');
-
   rdmCoverBtn.classList.add('hidden');
   saveCoverBtn.classList.add('hidden');
   homeBtn.classList.remove('hidden');
-
   displaySavedCovers();
 };
 
@@ -89,14 +74,13 @@ function switchViewsToHome() {
   homeView.classList.remove('hidden');
   savedCoversView.classList.add('hidden');
   makeMyBookView.classList.add('hidden');
-
   rdmCoverBtn.classList.remove('hidden');
   saveCoverBtn.classList.remove('hidden');
   homeBtn.classList.add('hidden');
 };
 
 function saveUserInput() {
-  // event.preventDefault();
+  event.preventDefault();
   covers.push(userCover.value);
   titles.push(userTitle.value);
   descriptors.push(userDescriptor1.value);
@@ -125,8 +109,7 @@ function displaySavedCovers() {
   }
 };
 
-function saveUserCover() {
-  event.preventDefault();
+function saveCover() {
   if (!savedCovers.includes(currentCover)) {
     savedCovers.push(currentCover);
   }
